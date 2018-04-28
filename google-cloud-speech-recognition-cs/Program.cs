3aa78da85840b05971c80fe3f5f28550ed34846c
okamoto <okamoto@nagao.nuie.nagoya-u.ac.jp>
2018-04-28 17:06:29 +0900

    add sample of google cloud speech

---
+++ google-cloud-speech-recognition-cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using google_cloud_speech_recognition_cs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace google_cloud_speech_recognition_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //recognize wav file
+            string audioFilePath = "audio file path";
+            string credentialsFilePath = "credentials file path(json)";
+            int samplingRate = 16000;
+            Recognize.AsyncRecognizeGcsWordsWithCredentials(audioFilePath, credentialsFilePath,samplingRate);
+
+            //mic reccognition
+            int recognitionTime = 100;
+            while (true)
+            {
+                var temp = Recognize.StreamingMicRecognizeAsyncWithCredentials(recognitionTime, credentialsFilePath).Result;
+            }
+
+            //tcp streaming recognition (implement in the near future)
+        }
+    }
+}
